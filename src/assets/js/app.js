ceb3fa0ec6956932ab8e41de911b5220b92db812
Binoop M J <binoop.mj@fayausa.com>
2016-08-29 13:01:44 +0530

    inner page created

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation();
+
+$(document).ready(function() {
+    console.log("ready!");
+});
+$( "#nav-toggle" ).on( "click", function() {
+	$(this).toggleClass("active");
+  $('#side-menu').toggle();
+});
+
+// var $div = $('#side-menu');
+// $(document.body).click(function(){
+//     if (!$div.has(this).length) { // if the click was not within $div
+//         $div.hide();
+//     }
+// });
