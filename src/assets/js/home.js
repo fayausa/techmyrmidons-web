2ca8c63c6157238ee95d6ac9eafbe96bd82679bf
Shameer B <shameer.b@fayausa.com>
2018-01-22 17:32:43 +0530

    Technology logo resize issue fixed.

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -5,7 +5,7 @@ $(function() {
 
     $.each(data.home, function(key, value) {
 
-    	var techRow = "<li  class='tech-link'><a class='techlist__item hovicon effect-4 sub-b' href='view.html?tech="+value.folderName +"'><img src='" +'data/'+ value.folderName +'/'+ value.folderName +'/' + value.logo + " ' alt=' " + value.folderName + " ' ><span> " + value.technology + " </span></a></li>"
+    	var techRow = "<li  class='tech-link'><a class='techlist__item hovicon effect-4 sub-b' href='view.html?tech="+value.folderName +"'><img width='80' src='" +'data/'+ value.folderName +'/'+ value.folderName +'/' + value.logo + " ' alt=' " + value.folderName + " '><span> " + value.technology + " </span></a></li>"
 
       $(techRow).appendTo("#technology-wrap");
     });
