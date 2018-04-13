b0f82c5da8f4493d169276a2697e7ee7deb3d9d4
Deepu S Nath <deepu@fayausa.com>
2018-04-13 18:03:53 +0530

    all technologies except ai

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,19 +1,18 @@
-$(function() { 
+$(function() {
   var url_string = location.href; //window.location.href
   var url = new URL(url_string);
   var tech = url.searchParams.get("tech");
-
   var title = tech + ' Myrmidon';
   $('.page-title').html(title);
 
 
-  // People to Follow 
+  // People to Follow
 
-  $.getJSON('/data/'+ tech +'/' + tech + '-follow.json', function(data) {
+  $.getJSON('/data/'+ tech +'/' + 'follow.json', function(data) {
 
-    $.each(data[tech], function(i, f) {
+    $.each(data['follow'], function(i, f) {
 
-      var follow = "<a href='" + f.followLink + "' target='blank' class='follow__list'><div class='follow__list__img grow'><img src="+'data/'+ tech +'/'+tech+'/' + f.photo +"></div><div class='follow__list__name'> <span>" + f.name + "</span> </div></a>"
+      var follow = "<a href='" + f.followLink + "' target='blank' class='follow__list'><div class='follow__list__img grow'><img src="+'data/'+ tech +'/images/' + f.photo +"></div><div class='follow__list__name'> <span>" + f.name + "</span> </div></a>"
 
       $(follow).appendTo("#follow");
 
@@ -24,9 +23,9 @@ $(function() {
 
   // Blog
 
-  $.getJSON('/data/'+ tech +'/' + tech + '-blog.json', function(data) {
+  $.getJSON('/data/'+ tech +'/' + 'blog.json', function(data) {
 
-    $.each(data[tech], function(i, f) {
+    $.each(data['blog'], function(i, f) {
 
       var androidBlog = "<a href='" + f.blog + "' target='blank' class='blog__name'>" + f.author + "</a>"
 
@@ -39,10 +38,10 @@ $(function() {
 
   //Technology Year Tab
 
-  $.getJSON('/data/'+ tech +'/'+ tech +'-years.json', function(data) {
+  $.getJSON('/data/'+ tech +'/'+ 'years.json', function(data) {
 
     var count = 0;
-    $.each(data[tech], function(i, f) {
+    $.each(data['years'], function(i, f) {
 
       var androidYear = "<li class='tab' data-year='"+ f.year +"' data-url='" + f.url + "'><a  href='"+ '#' + f.year + "'>" + f.year + "</a></li>"
 
@@ -52,13 +51,13 @@ $(function() {
 
       $(androidYeartab).appendTo("#yearTab");
       count = count + 1;
-      if (data[tech].length == count) {
+      if (data['years'].length == count) {
         getInitialData();
       }
     });
 
     $('#tab-container').easytabs();
-     
+
   });
 
   $('body').on('click','.tab',function(){
@@ -72,13 +71,12 @@ $(function() {
 
   function getJson(url, year) {
     $.getJSON(url, function(data) {
-      $("#detailPop").html('');
-      $("#" + year).html('');
+
       $.each(data[year], function(i, f) {
 
-        var techRow = "<div class='large-3 medium-6 columns'><div class='techwidget'><div class='techwidget__head'>" + f.title + "</div><p class='techwidget__content'>" + f.description + "</p><a class='techwidget__more popup-link' href='"+ '#' + f.morelink + "'>Read More</a></div></div>"
+        var techRow = "<div class='large-3 medium-6 columns'><div class='techwidget'><div class='techwidget__head'>" + f.title + "</div><p class='techwidget__content'>" + f.description + "</p><a class='techwidget__more popup-link' href='"+ '#' + f.title.replace(/\W/g, '') + "'>Read More</a></div></div>"
 
-        var techRowDetail = "<div id='"+ f.morelink + "' class='popup'><div class='techwidget'><div class='techwidget__head'>" + f.title + "</div><div class='techwidget__content'>" + f.description + "</div><p class='techwidget__content'>Dowoload here <a class='link' target='blank' href='" + f.url + "'> " + f.url + "</a></p></div></div>"
+        var techRowDetail = "<div id='"+ f.title.replace(/\W/g, '') + "' class='popup'><div class='techwidget'><div class='techwidget__head'>" + f.title + "</div><div class='techwidget__content'>" + f.description + "</div><p class='techwidget__content'>Know more about "+ f.title +"? <br> <a class='link' target='blank' href='" + f.url + "'> " + f.url + "</a></p></div></div>"
 
         $(techRowDetail).appendTo("#detailPop");
         $(techRow).appendTo("#" + year);
@@ -91,18 +89,15 @@ $(function() {
 
   function getInitialData() {
       var loadYear = window.location.hash.substr(1);
+
       if(loadYear){
-        var loadUrl  = '/data/'+tech+'/'+tech+'-'+ loadYear +'.json';
+        var loadUrl  = '/data/'+tech+'/'+ loadYear +'.json';
       } else {
         var loadUrl = $('.tab-container').find('.tab').attr('data-url');
         var loadYear = $('.tab-container').find('.tab').attr('data-year');
-      }     
+      }
       getJson(loadUrl, loadYear);
-   
+
   }
 
 });
-
-
-
-
