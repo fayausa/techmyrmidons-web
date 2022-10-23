da79c143c9f15a45dc8fa9074a0c33aa72b31d18
Aswin Asok <aswinasokofficial@gmail.com>
2022-10-23 07:34:55 +0530

    Minor Changes

--- src/DomainDetails/DomainDetails.jsx
+++ src/DomainDetails/DomainDetails.jsx
@@ -10,22 +10,22 @@ const DomainDetails = () => {
   return (
     <>
       <header>
-        <div className='container'>
+        <div className="container">
           <h1>Android Myrmidon</h1>
-          <p className='tagline'>
+          <p className="tagline">
             The best guide for you to start the next world Search Now
           </p>
-          <form action='' className='search'>
-            <button type='submit'></button>
-            <input type='text' placeholder='Search Now' />
+          <form action="" className="search">
+            <button type="submit"></button>
+            <input type="text" placeholder="Search Now" />
           </form>
         </div>
       </header>
 
       <main>
-        <section id='topics'>
-          <div className='container'>
-            <div className='cards-wrapper'>
+        <section id="topics">
+          <div className="container">
+            <div className="cards-wrapper">
               {Topics2021["2021"].map((topic) => (
                 <Card
                   title={topic.title}
@@ -38,42 +38,42 @@ const DomainDetails = () => {
           </div>
         </section>
 
-        <section id='blogs'>
-          <div className='container'>
-            <h2 className='section-title'>Blogs to follow</h2>
-            <div className='cards-wrapper'>
+        <section id="blogs">
+          <div className="container">
+            <h2 className="section-title">Blogs to follow</h2>
+            <div className="cards-wrapper">
               <Card
                 title="We've officially released our new app. meh!"
-                linkText='Read Now'
+                linkText="Read Now"
               />
               <Card
                 title="We've officially released our new app. meh!"
-                linkText='Read Now'
+                linkText="Read Now"
               />
               <Card
                 title="We've officially released our new app. meh!"
-                linkText='Read Now'
+                linkText="Read Now"
               />
               <Card
                 title="We've officially released our new app. meh!"
-                linkText='Read Now'
+                linkText="Read Now"
               />
               <Card
                 title="We've officially released our new app. meh!"
-                linkText='Read Now'
+                linkText="Read Now"
               />
               <Card
                 title="We've officially released our new app. meh!"
-                linkText='Read Now'
+                linkText="Read Now"
               />
             </div>
             <SectionBtn />
           </div>
         </section>
-        <section id='people'>
-          <div className='container'>
-            <h2 className='section-title'>People to follow</h2>
-            <div className='cards-wrapper'>
+        <section id="people">
+          <div className="container">
+            <h2 className="section-title">People to follow</h2>
+            <div className="cards-wrapper">
               {People.follow.map((person) => (
                 <Card
                   photo={person.photo}
