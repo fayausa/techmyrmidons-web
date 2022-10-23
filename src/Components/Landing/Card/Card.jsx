213fb01936d87bc4f8b34249e893bb66ee3c31f1
Aswin Asok <aswinasokofficial@gmail.com>
2022-10-23 12:40:31 +0530

    Data Updation

--- src/Components/Landing/Card/Card.jsx
+++ src/Components/Landing/Card/Card.jsx
@@ -3,17 +3,14 @@ import styles from "./Card.module.css";
 
 import { Link } from "react-router-dom";
 
-const Card = ({ name, image, link }) => {
+const Card = ({ name, image, link, description }) => {
   return (
     <div className={styles.card}>
       <div className={styles.card_image}>
         <img src={image} alt="" className={styles.card_img} />
       </div>
       <p className={styles.card_heading}>{name}</p>
-      <p className={styles.card_description}>
-        Designed to give you a comprehensive understanding of the current
-        Android ecosystem, resouces & learning paths.
-      </p>
+      <p className={styles.card_description}>{description}</p>
       <Link to={link}>
         <span className={styles.explore_more}>Explore More </span>
         <svg
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -42,40 +42,60 @@ const Landing = () => {
             image="/assets/landing/android.png"
             name="Android"
             link="/android"
+            description="Android is a mobile operating system based on a modified version of the Linux kernel and other open-source software."
           />
-          <Card image="/assets/landing/ios.png" name="iOS" link="/ios" />
+          <Card
+            image="/assets/landing/ios.png"
+            name="iOS"
+            link="/ios"
+            description="iOS is a mobile operating system created and developed by Apple Inc. exclusively for its hardware."
+          />
           <Card
             image="/assets/landing/devops.png"
             name="DevOps"
             link="/android"
+            description="DevOps is a set of practices that combines software development and IT operations."
           />
           <Card
             image="/assets/landing/frontend.png"
             name="Front-End"
             link="/frontend"
+            description="
+            The term Front-End mainly referes to the User Interface which an user view and interacts with and its working"
           />
-          {/* <Card
-            image="/assets/landing/backend.png"
-            name="Back-End"
-            link="/android"
-          /> */}
           <Card
             image="/assets/landing/blockchain.png"
             name="Blockchain"
             link="/blockchain"
+            description="
+            A blockchain is a digital ledger or database where encrypted blocks of digital asset data are stored and chained together."
           />
           <Card
             image="/assets/landing/ai.png"
             name="Articial Intelligence"
             link="/ai"
+            description="AI which stands for artificial intelligence refers to systems or machines that mimic human intelligence to perform tasks."
           />
-          <Card image="/assets/landing/arvr.png" name="AR/VR" link="/arvr" />
+          <Card
+            image="/assets/landing/arvr.png"
+            name="AR/VR"
+            link="/arvr"
+            description="
+            Augmented and virtual reality (AR/VR) are immersive technologies that enable users to experience digitally rendered content."
+          />
           <Card
             image="/assets/landing/google.png"
             name="Actions On Google"
             link="/actions-on-google"
+            description="
+            The Actions console is the web-based tool used for developing Actions for registration, deployment, configuration, and analytics."
           />
-          <Card image="/assets/landing/qa.png" name="QA" link="/qa" />
+          <Card
+            image="/assets/landing/qa.png"
+            name="QA"
+            link="/qa"
+            description="Quality assurance (QA) is any systematic process of determining whether a product or service meets specified requirements."
+          />
         </div>
       </div>
 
