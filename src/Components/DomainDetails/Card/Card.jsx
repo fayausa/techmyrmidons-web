bb228b6b78525c743cf802241a2edc4ae043d815
Aswin Asok <aswinasokofficial@gmail.com>
2022-10-23 23:38:54 +0530

    Scroll Bug Fix

--- src/Components/DomainDetails/Card/Card.jsx
+++ src/Components/DomainDetails/Card/Card.jsx
@@ -17,10 +17,11 @@ const Card = ({
   } else {
     document.body.classList.remove("active-modal");
   }
+
   return (
-    <div className='topic-card'>
+    <div className="topic-card">
       {photo && (
-        <div className='img-wrapper'>
+        <div className="img-wrapper">
           <img
             src={require(`../../../data/${id}/images/${photo}`)}
             onError={({ currentTarget }) => {
@@ -34,42 +35,40 @@ const Card = ({
       {desc && <p>{desc}</p>}
       {desc && (
         <div>
-          <span onClick={() => setModal(true)} className='link-text'>
+          <span onClick={() => setModal(true)} className="link-text">
             {linkText}
           </span>
 
           <svg
-            className='svg-image'
-            width='12'
-            height='12'
-            viewBox='0 0 15 15'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
+            className="svg-image"
+            width="12"
+            height="12"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              d='M12.1711 3.983L2.12965 14.0245L0.47998 12.3748L10.5203 2.33333H1.67115V0H14.5045V12.8333H12.1711V3.983Z'
-              fill='#000738'
+              d="M12.1711 3.983L2.12965 14.0245L0.47998 12.3748L10.5203 2.33333H1.67115V0H14.5045V12.8333H12.1711V3.983Z"
+              fill="#000738"
             />
           </svg>
         </div>
       )}
       {!desc && link && (
-        <a href={link} target='_blank' rel='noopener noreferrer'>
+        <a href={link} target="_blank" rel="noopener noreferrer">
           <div>
-            <span onClick={() => setModal(true)} className='link-text'>
-              {linkText}
-            </span>
+            <span className="link-text">{linkText}</span>
             <svg
-              className='svg-image'
-              width='12'
-              height='12'
-              viewBox='0 0 15 15'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
+              className="svg-image"
+              width="12"
+              height="12"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                d='M12.1711 3.983L2.12965 14.0245L0.47998 12.3748L10.5203 2.33333H1.67115V0H14.5045V12.8333H12.1711V3.983Z'
-                fill='#000738'
+                d="M12.1711 3.983L2.12965 14.0245L0.47998 12.3748L10.5203 2.33333H1.67115V0H14.5045V12.8333H12.1711V3.983Z"
+                fill="#000738"
               />
             </svg>
           </div>
